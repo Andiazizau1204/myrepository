e5532e69eb4d30230a7b2c6364ec2f59a980f6b8
Andiazizau1204 <70931694+Andiazizau1204@users.noreply.github.com>
2020-09-08 11:28:57 +0700

    Ini adalah ulangan java, tentang String kelas 12

---
+++ ulangan.java
@@ -0,0 +1,25 @@
+package StringOOP;
+
+public class ulangan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+	String[] nama = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q",
+			         "R","S","T","U","V","W","X","Y","Z","#","-"};
+	
+	String n1 = new String(nama[0]);
+	String n2 = new String(nama[1]);
+	String n3 = new String(nama[4]);
+	String n4 = new String(nama[11]);
+	String n5 = new String(nama[17]);
+	String n6 = new String(nama[19]);
+	String n7 = new String(nama[20]);
+	String n8 = new String(nama[27]);
+	
+	
+	
+	System.out.println(n5+n1+n6+n7+n8+n2+n3+n4+n4+n1);
+}
+}
